42d7d035638389a3549f4cbf663d5d8b51f7bff5
BrianS99 <61093495+BrianS99@users.noreply.github.com>
2021-07-07 21:11:51 -0700

    Store can skeleton code

--- toucan_software/src/fsm.cpp
+++ toucan_software/src/fsm.cpp
@@ -1,42 +1,112 @@
 #include "fsm.h"
 
-void check_state() 
+volatile int reservoir_state = 0;
+
+void check_state()
 {
-    static enum {INITIALIZE, SEARCH, GRAB_CAN, STORE_CAN, ALIGN, DROPOFF} state = INITIALIZE;
+    static enum { INITIALIZE,
+                  SEARCH,
+                  GRAB_CAN,
+                  STORE_CAN,
+                  ALIGN,
+                  DROPOFF } state = INITIALIZE;
     static uint32_t time_elapsed = 0;
 
-    switch(state) 
+    switch (state)
     {
-        case INITIALIZE:
+    case INITIALIZE:
         // start-up sequence / waiting for the robot to touch ground, use tape sensors for this
-            break;
-        case SEARCH:
+        break;
+    case SEARCH:
         // has initialized, stored a can, or completed drop-off -> follow tape, flapper on
-            break;
+        break;
 
-        case GRAB_CAN:
+    case GRAB_CAN:
         // can has been sensed -> flapper off, stop driving, grab can
-            break;
+        break;
 
-        case STORE_CAN:
+    case STORE_CAN:
         // can grabbed -> store can in reservoir
         // need memory for which reservoir slot to use? -> use global variable or something
         // TODO: Brian
-            break;
+        boolean can_stored = store_can();
+        if (can_stored)
+        {
+            if (reservoir_state < 2)
+            {
+                reservoir_state++;
+            }
+            else
+            {
+                reservoir_state = 0;
+            }
+        }
 
-        case ALIGN:
+        break;
+
+    case ALIGN:
         // reached return vehicle -> stop flapper, change driving somehow, line up next to it
-            break;
+        break;
 
-        case DROPOFF:
+    case DROPOFF:
         // has aligned to the return vehicle
         // if no servo -> drive forwards slowly to drop cans, sense when the robot has reached the end of the return vehicle
         // if servo -> open the reservoir
-            break;
-        
-        default:
-            state = INITIALIZE;
-            break;
+        break;
+
+    default:
+        state = INITIALIZE;
+        break;
     }
 }
 
+boolean store_can()
+{
+    delay(1000);
+
+    for (int i = ARM_DOWN; i < ARM_UP; i += 25)
+    {
+        pwm_start(ARM_SERVO, MOTOR_FREQ, i, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+        delay(15);
+    }
+
+    delay(1000);
+
+    for (int i = SWIVEL_ORIGIN; i > RESERVOIR_POSITIONS[reservoir_state]; i -= 50)
+    {
+        pwm_start(SWIVEL_SERVO, MOTOR_FREQ, i, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+        delay(30);
+    }
+
+    delay(1000);
+
+    pwm_start(CLAW_SERVO, MOTOR_FREQ, CLAW_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+
+    delay(1000);
+
+    for (int i = RESERVOIR_POSITIONS[reservoir_state]; i < SWIVEL_ORIGIN; i += 50)
+    {
+        pwm_start(SWIVEL_SERVO, MOTOR_FREQ, i, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+        delay(30);
+    }
+
+    delay(1000);
+
+    pwm_start(CLAW_SERVO, MOTOR_FREQ, CLAW_CLOSE, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+
+    delay(1000);
+
+    for (int i = ARM_UP; i > ARM_DOWN; i -= 25)
+    {
+        pwm_start(ARM_SERVO, MOTOR_FREQ, i, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+        delay(15);
+    }
+
+    delay(1000);
+
+    pwm_start(CLAW_SERVO, MOTOR_FREQ, CLAW_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+
+    delay(1000);
+
+    return true;
+}
