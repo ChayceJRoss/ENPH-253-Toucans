07f711693f00b09f6b293d8cd0e870ad82f8cac8
tarakong01 <tarakong01@gmail.com>
2021-07-08 14:22:01 -0700

    added adafruit for debugging

--- toucan_software/src/fsm.cpp
+++ toucan_software/src/fsm.cpp
@@ -1,4 +1,7 @@
 #include "fsm.h"
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+volatile int init_time_sensed = 0;
 
 void drive()
 {
@@ -7,10 +10,25 @@ void drive()
 
 bool search()
 {
+    delay(2000);
+    return true;
     drive();
     // start flapper
     pwm_start(FLAPPER_MOTOR, MOTOR_FREQ, DC_FREQ, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-    if (analogRead(CLAW_SENSOR) < CAN_SENSING_THRESHOLD)
+    bool can_sensed = false;
+    if(analogRead(CLAW_SENSOR) < CAN_SENSING_THRESHOLD){
+        if (init_time_sensed == 0)
+        {
+            init_time_sensed = millis();
+        }
+        can_sensed = true;
+    }
+    else
+    {
+        init_time_sensed = 0;
+        can_sensed = false;
+    }
+    if (analogRead(CLAW_SENSOR) < CAN_SENSING_THRESHOLD && can_sensed && (millis() - init_time_sensed > TIME_TO_GRAB_CAN_THRESHOLD))
     {
         // shut off flapper
         pwm_start(FLAPPER_MOTOR, MOTOR_FREQ, 0, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
@@ -20,7 +38,7 @@ bool search()
         pwm_start(LEFT_WHEEL_B, MOTOR_FREQ, 0, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
         pwm_start(RIGHT_WHEEL_A, MOTOR_FREQ, 0, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
         pwm_start(RIGHT_WHEEL_B, MOTOR_FREQ, 0, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-        delay(50);
+        // delay(50);
         
         return true;
     }
@@ -31,7 +49,9 @@ bool grab_can()
 { 
     // close claw
     pwm_start(CLAW_SERVO, MOTOR_FREQ, CLAW_CLOSE, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-    delay(50);
+    delay(200);
+    pwm_start(CLAW_SERVO, MOTOR_FREQ, CLAW_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+    delay(200);
 
     // check reflectance -> or change this to a switch reading
     if (analogRead(CLAW_SENSOR) < CAN_SENSING_THRESHOLD)
@@ -41,15 +61,19 @@ bool grab_can()
     return false;
 }
 
-void check_state() 
-{
+void check_state()
+{   
     static enum {INITIALIZE, SEARCH, GRAB_CAN, STORE_CAN, ALIGN, DROPOFF} state = INITIALIZE;
-    static uint32_t time_elapsed = 0;
-
-    switch(state) 
+    display.println("state: ");
+    display.println(state);
+    display.println("reflectance: ");
+    display.println(analogRead(CLAW_SENSOR));
+    switch(state)
     {
         case INITIALIZE:
         // start-up sequence / waiting for the robot to touch ground, use tape sensors for this
+            delay(2000);
+            state = SEARCH;
             break;
         case SEARCH:
         // has initialized, stored a can, or completed drop-off -> follow tape, flapper on
@@ -70,6 +94,7 @@ void check_state()
         case STORE_CAN:
         // can grabbed -> store can in reservoir
         // need memory for which reservoir slot to use? -> use global variable or something
+            
             break;
 
         case ALIGN:
@@ -87,4 +112,3 @@ void check_state()
             break;
     }
 }
-
--- toucan_software/src/fsm.h
+++ toucan_software/src/fsm.h
@@ -3,6 +3,22 @@
 #include <Wire.h>
 #include "constants.h"
 
+/** Drives the robot. Contains PID code
+ */
+void drive();
+
+/** searches the floor for cans
+ *  returns true if a can has been sensed in front of the claw, false otherwise
+ */
+bool search();
+
+/** grabs a can
+ *  returns true if the can was grabbed, false otherwise
+ */
+bool grab_can();
+
 /** checks the state of the FSM and executes the state
  */
 void check_state();
+
+extern Adafruit_SSD1306 display;
