4d116a456b13b6725268586d706f9d1ff0941fd6
BrianS99 <61093495+BrianS99@users.noreply.github.com>
2021-07-22 18:47:57 -0700

    edits

--- toucan_software/src/constants.h
+++ toucan_software/src/constants.h
@@ -22,7 +22,7 @@
 #define FLAPPER_MOTOR PB_9
 
 // blake 1.0, tara 1.1, brian 0.9
-const double RW_ADJUSTMENT_FACTOR = 1.1;
+const double RW_ADJUSTMENT_FACTOR = 1.0;
 const int MAX_INTEGRATOR_VALUE = 100;
 
 // Adafruit Display: PB6, PB7
